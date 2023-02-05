d80031617c03909b497fda9a881af9bc6a50664b
Ayosef <syrivera@uc.cl>
2023-02-05 12:06:39 -0300

    Add the grid layout and the function to create it

---
+++ index.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+
+function getGrid() {
+    for (let i = 0; i < 16; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < 16; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+}
